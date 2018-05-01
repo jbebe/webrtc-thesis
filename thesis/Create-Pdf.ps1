7794b82c26f5daf671526aa6296d56f2229b8fe0
jbebe <juhasz.balint.bebe@gmail.com>
2018-05-02 00:46:44 +0200

    Readable changes for document

--- thesis/Create-Pdf.ps1
+++ thesis/Create-Pdf.ps1
@@ -1,11 +1,56 @@
 #requires -version 2
 
+function GetXml {
+	[xml]$xml = Get-Content $xmlPath -Encoding UTF8
+	[System.Xml.XmlNamespaceManager]$ns = $xml.NameTable
+	$ns.AddNamespace('w','http://schemas.openxmlformats.org/wordprocessingml/2006/main')
+	$xml.PreserveWhitespace = $true
+	$queryNonEmptyTextNodes = '//w:t[string-length(text()) > 0 and not(text()[not(normalize-space())])]/node()'
+	@{
+		Nodes=$xml.SelectNodes($queryNonEmptyTextNodes, $ns)
+		Handle=$xml
+	}
+}
+
+function MergeFragments {
+	$lines = [string[]](Get-Content -Path $textPath)
+	
+	$counter = 0
+	$xml = GetXml
+	foreach ($node in $xml.Nodes){
+		$node.Value = $lines[$counter]
+		$counter += 1
+	}
+
+	Write-Host "Update document.xml"
+	$xml.Handle.Save($xmlPath)
+	Write-Host "Delete raw.txt"
+	Remove-Item -Path $textPath
+}
+
+function CreateFragments {
+	$lines = [System.Collections.Generic.List[System.String]]@()
+	$xml = GetXml
+	foreach ($node in $xml.Nodes){
+		$val = $node.Value
+		$lines.Add($val)
+		$node.Value = "$"
+	}
+
+	Set-Content -Value ([System.String]::Join("`n", $lines)) -Path $textPath -Encoding UTF8
+	Write-Host "Update document.xml"
+	$xml.Handle.Save($xmlPath)
+}
+
 $ErrorActionPreference = "Stop"
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
 $folder = Join-Path -Path $pwd -ChildPath "source"
 $docx = Join-Path -Path $pwd -ChildPath "thesis.docx"
 $pdf = Join-Path -Path $pwd -ChildPath "thesis.pdf"
+
+Write-Host "Load text from raw.txt to document.xml"
+MergeFragments
 
 Write-Host "Creating docx from folder."
 [System.IO.Compression.ZipFile]::CreateFromDirectory($folder, $docx)
@@ -19,3 +64,6 @@ $wordApp.Quit()
 
 Write-Host "Remove docx file."
 Remove-Item -Path $docx
+
+Write-Host "Creating raw.txt and filling document.xml with placeholders"
+CreateFragments
